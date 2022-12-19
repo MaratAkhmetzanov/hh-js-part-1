cddb7c28f89ee8934b86f18bbb86a06e0f4e92b0
MaratAkhmetzanov <ahmetzanovmarat@gmail.com>
2022-12-19 22:48:46 +0300

    find route algorithm, getters

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const API_URL = 'https://restcountries.com/v3.1';
+
 async function getData(url) {
     // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
     const response = await fetch(url, {
@@ -11,11 +13,83 @@ async function getData(url) {
 }
 
 async function loadCountriesData() {
-    const countries = await getData('https://restcountries.com/v3.1/all?fields=name&fields=cca3&fields=area');
+    const countries = await getData(`${API_URL}/all?fields=name&fields=cca3&fields=area`);
     return countries.reduce((result, country) => {
         result[country.cca3] = country;
         return result;
     }, {});
+}
+
+async function getCodeByName(name) {
+    const result = await getData(`${API_URL}/name/${name}?fullText=true&fields=cca3&fields=borders&fields=name`);
+    return result.status ? result : result[0];
+}
+
+async function getBordersByCode(code) {
+    if (code === 'BRN') {
+        const result = await getData(`${API_URL}/name/Brunei?fullText=true&fields=cca3&fields=borders&fields=name`);
+        return result[0];
+    }
+    const result = await getData(`${API_URL}/alpha/${code}?fields=cca3&fields=borders&fields=name`);
+    return result;
+}
+
+async function findRoute(fromCountry, toCountry) {
+    const errorResult = (message) =>
+        Promise.reject({
+            status: 'error',
+            message,
+        });
+
+    const startCountry = await getCodeByName(fromCountry);
+    const endCountry = await getCodeByName(toCountry);
+
+    if (startCountry.status) {
+        return errorResult('Unable to calculate route. Cannot find "from" country');
+    }
+    if (endCountry.status) {
+        return errorResult('Unable to calculate route. Cannot find "to" country');
+    }
+    if (!startCountry.borders.length || !endCountry.borders.length) {
+        return errorResult('No ground route between countries');
+    }
+
+    let requestCounter = 2;
+    const visited = [];
+    const routes = [];
+    const stack = [...startCountry.borders];
+    routes[startCountry.cca3] = [startCountry.name.common];
+
+    while (stack.length) {
+        // eslint-disable-next-line no-await-in-loop
+        const country = await getBordersByCode(stack.shift());
+        requestCounter += 1;
+        visited[country.cca3] = 1;
+        let minDestination = Infinity;
+
+        country.borders.forEach((element) => {
+            if (!stack.includes(element) && !visited[element]) {
+                stack.push(element);
+            }
+            if (routes[element] && minDestination >= routes[element].length) {
+                routes[country.cca3] = [...routes[element], country.name.common];
+                minDestination = routes[element].length;
+            }
+        });
+
+        if (country.borders.includes(endCountry.cca3)) {
+            minDestination = Infinity;
+
+            endCountry.borders.forEach((element) => {
+                if (routes[element] && minDestination >= routes[element].length) {
+                    routes[endCountry.cca3] = [...routes[element], endCountry.name.common];
+                    minDestination = routes[element].length;
+                }
+            });
+            return { status: 'ok', requestCount: requestCounter, route: routes[endCountry.cca3] };
+        }
+    }
+    return errorResult('No ground route between countries');
 }
 
 const form = document.getElementById('form');
@@ -36,7 +110,17 @@ const output = document.getElementById('output');
 
     // Заполняем список стран для подсказки в инпутах
     Object.keys(countriesData)
-        .sort((a, b) => countriesData[b].area - countriesData[a].area)
+        .sort((a, b) => {
+            const nameA = countriesData[a].name.common;
+            const nameB = countriesData[b].name.common;
+            if (nameA < nameB) {
+                return -1;
+            }
+            if (nameA > nameB) {
+                return 1;
+            }
+            return 0;
+        })
         .forEach((code) => {
             const option = document.createElement('option');
             option.value = countriesData[code].name.common;
@@ -49,8 +133,34 @@ const output = document.getElementById('output');
 
     form.addEventListener('submit', (event) => {
         event.preventDefault();
-        // TODO: Вывести, откуда и куда едем, и что идёт расчёт.
-        // TODO: Рассчитать маршрут из одной страны в другую за минимум запросов.
-        // TODO: Вывести маршрут и общее количество запросов.
+        if (fromCountry.value && toCountry.value) {
+            fromCountry.disabled = true;
+            toCountry.disabled = true;
+            submit.disabled = true;
+            output.textContent = `Calculating path from ${fromCountry.value} to ${toCountry.value} …`;
+
+            findRoute(fromCountry.value, toCountry.value)
+                .then((res) => {
+                    if (res.status === 'ok' && res.route.length) {
+                        output.innerHTML = `Request count: ${res.requestCount} <br />`;
+                        output.innerHTML += `Countries in route: ${res.route.length} <br />`;
+                        output.innerHTML += `Route: ${res.route.join(' → ')}`;
+                    }
+                })
+                .catch((error) => {
+                    if (error.message) {
+                        output.innerHTML = error.message;
+                    } else {
+                        output.innerHTML = 'Unknown error';
+                    }
+                })
+                .finally(() => {
+                    fromCountry.disabled = false;
+                    toCountry.disabled = false;
+                    submit.disabled = false;
+                });
+        } else {
+            output.textContent = 'Please, select both countries';
+        }
     });
 })();
